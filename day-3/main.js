5a80d6cf1f9652122cf87095c6ebcab67a80e697
evaferrerasbr <evaferrerasbr@gmail.com>
2020-12-05 22:13:58 +0100

    End part 1 using module to search in the complete map

---
+++ day-3/main.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const forest = fs
+  .readFileSync('data.txt', { encoding: 'utf-8' })
+  .split(/\r?\n|\r/g);
+
+//part 1
+let counter = 0;
+
+for (let i = 0; i < forest.length; i++) {
+  const treeSearcher = i * 3;
+  const stringIndex = treeSearcher % forest[i].length;
+  if (forest[i][stringIndex] === '#') {
+    counter++;
+  }
+}
