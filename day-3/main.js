0fb3e62b232eb64bca359079d3744d990b29c0fa
evaferrerasbr <evaferrerasbr@gmail.com>
2020-12-07 15:52:07 +0100

    Add code comments

--- day-3/main.js
+++ day-3/main.js
@@ -5,17 +5,25 @@ const forest = fs
   .split(/\r?\n|\r/g);
 
 //part 1
-let counter = 0;
 
-for (let i = 0; i < forest.length; i++) {
-  const treeSearcher = i * 3;
-  const stringIndex = treeSearcher % forest[i].length;
-  if (forest[i][stringIndex] === '#') {
-    counter++;
+const findTrees = () => {
+  let counter = 0;
+  for (let i = 0; i < forest.length; i++) {
+    const treeSearcher = i * 3;
+    const stringIndex = treeSearcher % forest[i].length;
+    //uses module to "imagine" the forest pattern instead of creating it
+    if (forest[i][stringIndex] === '#') {
+      counter++;
+    }
   }
-}
+  return counter;
+};
+
+findTrees();
 
 //part 2
+
+const finalCounter = [];
 const slopes = [
   {
     right: 1,
@@ -39,22 +47,30 @@ const slopes = [
   },
 ];
 
-const finalCounter = [];
+//checks how big a step is in x and y exes and search trees for different slopes
+const findTreesWithSlopes = () => {
+  for (let i = 0; i < slopes.length; i++) {
+    let counter = 0;
+    let numberOfSteps = 0;
+    let verticalSteps = 0;
+    do {
+      verticalSteps = slopes[i].down * numberOfSteps;
+      const horizontalSteps = slopes[i].right * numberOfSteps;
+      const stringIndex = horizontalSteps % forest[verticalSteps].length;
+      if (forest[verticalSteps][stringIndex] === '#') {
+        counter++;
+      }
+      numberOfSteps++;
+    } while (verticalSteps + 1 < forest.length);
+    finalCounter.push(counter);
+  }
+};
 
-for (let i = 0; i < slopes.length; i++) {
-  let counter = 0;
-  let numberOfSteps = 0;
-  let verticalSteps = 0;
-  do {
-    verticalSteps = slopes[i].down * numberOfSteps;
-    const horizontalSteps = slopes[i].right * numberOfSteps;
-    const stringIndex = horizontalSteps % forest[verticalSteps].length;
-    if (forest[verticalSteps][stringIndex] === '#') {
-      counter++;
-    }
-    numberOfSteps++;
-  } while (verticalSteps + 1 < forest.length);
-  finalCounter.push(counter);
-}
+findTreesWithSlopes();
 
-const result = finalCounter.reduce((value, number) => value * number, 1);
+const getMult = () => {
+  const result = finalCounter.reduce((value, number) => value * number, 1);
+  return result;
+};
+
+getMult();
