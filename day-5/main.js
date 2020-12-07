2f8edd355a6624c6cefc3d2c1173e22ac6bc507d
evaferrerasbr <evaferrerasbr@gmail.com>
2020-12-07 10:05:18 +0100

    Add getId to operate with rows and columns

--- day-5/main.js
+++ day-5/main.js
@@ -9,15 +9,41 @@ const boardingPasses = fs
 
 const binaryPasses = [];
 
-const getBinaryFromPasses = boardingPasses.map((boardingPass) => {
-  const test = boardingPass
+for (let i = 0; i < boardingPasses.length; i++) {
+  const binaryPass = boardingPasses[i]
     .replace(/F/g, '0')
     .replace(/L/g, '0')
     .replace(/B/g, '1')
     .replace(/R/g, '1');
-  binaryPasses.push(test);
+  const pass = {
+    row: '',
+    column: '',
+  };
+  for (let j = 0; j < boardingPasses[i].length; j++) {
+    if (j <= 6) {
+      pass.row = pass.row.concat(binaryPass[j]);
+    } else {
+      pass.column = pass.row.concat(binaryPass[j]);
+    }
+  }
+  binaryPasses.push(pass);
+}
+
+const ids = [];
+
+const getIds = () => {
+  for (const binaryPass of binaryPasses) {
+    let row = parseInt(binaryPass.row, 2);
+    let column = parseInt(binaryPass.column, 2);
+    let id = row * 8 + column;
+    ids.push(id);
+  }
+};
+
+getIds();
+
+const sortedIds = ids.sort(function (a, b) {
+  return b - a;
 });
 
-console.log(binaryPasses);
-
-// console.log(parseInt(binaryPasses[0], 2));
+console.log(sortedIds);
